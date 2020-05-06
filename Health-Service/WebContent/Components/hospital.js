569315d013f843813300ebb03fad8954394c59bf
Prasad <Prasad@DESKTOP-3GG6746>
2020-05-06 14:52:27 +0530

    Hospital API Done

--- Health-Service/WebContent/Components/hospital.js
+++ Health-Service/WebContent/Components/hospital.js
@@ -29,8 +29,24 @@ $(document).on("click", "#btnSave", function(event)
   $("#alertError").show();
   return;
   }
+ 
+ 
+ var type = ($("#hideHospitalSave").val() == "") ? "POST" : "PUT";
+
+ $.ajax(
+	{
+	 url : "HospitalAPI",
+	 type : type,
+	 data : $("#form_hospital").serialize(),
+	 dataType : "text",
+	 complete : function(response, status)
+	 {
+		 onHospitalSaveComplete(response.responseText, status);
+	 }
+ });
+ 
  // If valid-------------------------
-  $("#form_hospital").submit();
+ // $("#form_hospital").submit();
  });
 
 
@@ -40,7 +56,7 @@ $(document).on("click", "#btnSave", function(event)
 //UPDATE==========================================
 $(document).on("click", ".btnUpdate", function(event)
 {
- $("#hideHospitalSave").val($(this).closest("tr").find('#hidhospitalIDUpdate').val());
+ $("#hideHospitalSave").val($(this).closest("tr").find('#hideHospitalSave').val());
  $("#hos_id").val($(this).closest("tr").find('td:eq(0)').text());
  $("#hos_name").val($(this).closest("tr").find('td:eq(1)').text());
  $("#hos_address").val($(this).closest("tr").find('td:eq(2)').text());
@@ -54,13 +70,20 @@ $(document).on("click", ".btnUpdate", function(event)
 
 
 // DELETE==========================================
-$(document).on("click", ".remove", function(event)
+$(document).on("click", ".btnRemove", function(event)
 {
- $(this).closest(".hospital").remove();
-
- $("#alertSuccess").text("Removed successfully.");
- $("#alertSuccess").show();
-});
+	$.ajax(
+	 {
+		 url : "HospitalAPI",
+		 type : "DELETE",
+		 data : "hos_id=" + $(this).data("hos_id"),
+		 dataType : "text",
+		 complete : function(response, status)
+		{
+		 onItemDeleteComplete(response.responseText, status);
+		 }
+	 });
+	});
 
 
 
@@ -79,14 +102,35 @@ if ($("#hos_address").val().trim() == "")
 return "Insert hospital address.";
 }
 
-if ($("#hos_phone").val().trim() == "")
+/*if ($("#hos_phone").val().trim() == "")
 {
 return "Insert hospital phone.";
+}*/
+// Validate numbers
+
+if ($("#hos_phone").val().trim() != ""){
+	
+	if($("#hos_phone").val().length < 10){
+		return "Wrong Phone Number";
+	}	
 }
 
-if ($("#hos_email").val().trim() == "")
+/*if ($("#hos_email").val().trim() == "")
 {
 return "Insert hospital email.";
+}*/
+// validate emails
+if ($("#hos_email").val().trim() != ""){
+	
+	var email = $("#hos_email").val();
+	var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	
+	if(!regex.test(email))
+	{
+		return "InValide Email.";
+	}
+		
+	
 }
 return true;
 }
@@ -110,3 +154,63 @@ hospital += "</div>";
 	return hospital;
 	}
 	
+
+
+
+
+function onHospitalSaveComplete(response, status)
+{
+if (status == "success")
+ {
+ var resultSet = JSON.parse(response);
+ if (resultSet.status.trim() == "success")
+ {
+ $("#alertSuccess").text("Successfully saved.");
+ $("#alertSuccess").show();
+ $("#divHospitalGrid").html(resultSet.data);
+ } else if (resultSet.status.trim() == "error")
+ {
+ $("#alertError").text(resultSet.data);
+ $("#alertError").show();
+ }
+ } else if (status == "error")
+ {
+ $("#alertError").text("Error while saving.");
+ $("#alertError").show();
+ } else
+ {
+ $("#alertError").text("Unknown error while saving..");
+ $("#alertError").show();
+ }
+ $("#hideHospitalSave").val("");
+ $("#form_hospital")[0].reset();
+}
+
+
+
+
+function onHospitalDeleteComplete(response, status)
+{
+if (status == "success")
+ {
+ var resultSet = JSON.parse(response);
+ if (resultSet.status.trim() == "success")
+ {
+ $("#alertSuccess").text("Successfully deleted.");
+ $("#alertSuccess").show();
+ $("#divHospitalGrid").html(resultSet.data);
+ } else if (resultSet.status.trim() == "error")
+ {
+ $("#alertError").text(resultSet.data);
+ $("#alertError").show();
+ }
+ } else if (status == "error")
+ {
+ $("#alertError").text("Error while deleting.");
+ $("#alertError").show();
+ } else
+ {
+ $("#alertError").text("Unknown error while deleting..");
+ $("#alertError").show();
+ }
+}
--- Health-Service/src/com/HospitalAPI.java
+++ Health-Service/src/com/HospitalAPI.java
@@ -1,6 +1,10 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HospitalAPI")
 public class HospitalAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	Hospital_DB hos_Obj = new Hospital_DB();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,22 +39,63 @@ public class HospitalAPI extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String output = hos_Obj.insertHospital(request.getParameter("hos_name"),
+				 request.getParameter("hos_address"),
+			 	 request.getParameter("hos_phone"),
+			 	 request.getParameter("hos_email"));
+		
+				response.getWriter().write(output); 
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+		Map paras = getParasMap(request);
+		 String output = hos_Obj.updateHospital(paras.get("hidItemIDSave").toString(),
+		 paras.get("hos_name").toString(),
+		 paras.get("hos_address").toString(),
+		paras.get("hos_phone").toString(),
+		paras.get("hos_email").toString());
+		response.getWriter().write(output);
+		}
+	
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+			 Map paras = getParasMap(request);
+			 String output = hos_Obj.deleteHospital(paras.get("hos_id").toString());
+			response.getWriter().write(output);
+			}
 
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
 
+	
+	
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request)
+	{
+	 Map<String, String> map = new HashMap<String, String>();
+	try
+	 {
+	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+	 String queryString = scanner.hasNext() ?
+	 scanner.useDelimiter("\\A").next() : "";
+	 scanner.close();
+	 String[] params = queryString.split("&");
+			 for (String param : params)
+			 {
+				 String[] p = param.split("=");
+				 map.put(p[0], p[1]);
+			}
+		 }
+		catch (Exception e)
+		 {
+		 }
+		return map;
+		}
+		 
+		 
+
+		 
+	 
 }
